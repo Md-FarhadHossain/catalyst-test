974cca90b7dc85e2181db42c9189166d4244ea59
Md-FarhadHossain <freelancerfarhadhossain@gmail.com>
2022-12-24 20:18:00 +0600

    cart item add

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { PhotoProvider } from '../../context/PhotoContext'
 
 const Cart = () => {
+  const {cartItems} = useContext(PhotoProvider)
+  console.log(cartItems)
   return (
-    <div>Cart</div>
+    <div>
+      {cartItems.length}
+      <div  className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4'>
+      {
+        cartItems.map(item => <div key={item.id}>
+          <img  src={item.url} />
+        </div>)
+      }
+      </div>
+    </div>
   )
 }
 
